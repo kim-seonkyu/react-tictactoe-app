f936c79f42c237a1f324dce12eb594ed3a15d4d7
kim-seonkyu <seonkyu93@gmail.com>
2023-02-21 03:24:41 +0900

    tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Board from "./components/Board";
 
 function App() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [isX, setIsX] = useState(true);
+  const [stepNum, setStepNum] = useState(0);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const current = history[stepNum];
+  const winner = calculateWinner(current.squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = `Next Player: ${isX ? "X" : "O"}`;
+  }
+
+  const handleClick = (i) => {
+    const newHistory = history.slice(0, stepNum + 1);
+    const newCurrent = newHistory[newHistory.length - 1];
+    const newSquares = newCurrent.squares.slice();
+
+    if (calculateWinner(newSquares) || newSquares[i]) {
+      return;
+    }
+    newSquares[i] = isX ? "X" : "O";
+    setHistory([...newHistory, { squares: newSquares }]);
+    setIsX((prev) => !prev);
+
+    setStepNum(newHistory.length);
+  };
+
+  const jumpTo = (step) => {
+    setStepNum(step);
+    setIsX(step % 2 === 0);
+  };
+
+  const moves = history.map((step, move) => {
+    const desc = move ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button className="move-btn" onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="game">
+        <div className="game-board">
+          <Board squares={current.squares} onClick={(i) => handleClick(i)} />
+        </div>
+        <div className="game-info">
+          <div className="status">{status}</div>
+          <ul>{moves}</ul>
+        </div>
+      </div>
     </div>
   );
 }
